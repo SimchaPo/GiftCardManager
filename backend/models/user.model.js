a510a3da0ef18643152a0eb0969b2999c733261d
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2021-04-12 09:15:04 +0300

    start building project

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        uniqe:true,
+        trim:true,
+        minlength:true
+    },
+},{
+    timestamps:true,
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
